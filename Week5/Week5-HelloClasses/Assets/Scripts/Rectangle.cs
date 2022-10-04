70934ec43bdbf7d22c6806c70ac8ae026f23bf6f
MylesC21 <113139790+MylesC21@users.noreply.github.com>
2022-10-04 16:29:23 -0400

    Updated
    Updated.gitignore

---
+++ Week5/Week5-HelloClasses/Assets/Scripts/Rectangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Rectangle
+{
+    private float height;
+    private float width;
+    private float length;
+
+    public Rectangle()
+    {
+        height = 0f;
+        width = 0f;
+        length = 0f;
+    }
+
+    public Rectangle(float newHeight, float newWidth)
+    {
+
+        height = newHeight;
+        width = newWidth;
+    }
+
+    public float GetPerimeter()
+    {
+        return 2 * height + 2 * width;
+    }
+
+    public float GetArea()
+    {
+        return height * width;
+    }
+
+    public float GetHeight()
+    {
+        return height;
+    }
+
+    public void SetHeight(float newHeight)
+    {
+        height = newHeight;
+    }
+
+    public float GetWidth()
+    {
+        return width;
+    }
+
+    public void SetWidth(float newWidth)
+    {
+        width = newWidth;
+    }
+    public float GetLength()
+    {
+        return Length;
+    }
+
+    public void SetLength(float newLength)
+    {
+        length = newLength;
